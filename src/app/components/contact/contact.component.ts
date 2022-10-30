8a0eeb543b946e0507fde35e7425171dd8180a63
Matei Nicolae - Daniel <matei1337@icloud.com>
2022-10-30 17:56:22 +0200

    contact and portofolio page done. added the last two buttons into the mobile navigation menu

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+// Font Awesome icons.
+import { faPhoneSquare } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import { faMap } from '@fortawesome/free-regular-svg-icons';
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent implements OnInit {
+  faPhoneSquare = faPhoneSquare;
+  faEnvelope = faEnvelope;
+  faMap = faMap;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
